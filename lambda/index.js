404785ad30bcb0fc6fe73c4e4d81db5219ca4659
Abraham Moran <moranabraham07@outlook.com>
2022-09-10 13:40:15 -0700

    consult api

--- lambda/index.js
+++ lambda/index.js
@@ -5,20 +5,20 @@
  * */
 const Alexa = require('ask-sdk-core');
 
-const getRemoteData = function(url) {
-    return new Promise((resolve, reject) => {
-        const client = url.startsWith('https') ? require('https'):require('http')
-        const request = client.get(url, (response) => {
-            if(response.statusCode < 200 || response.statusCode > 299){
-                reject(new Error('Failed with status code: '+response.statusCode))
-            }
-            const body = []
-            response.on('data', (chunk) => body.push(chunk))
-            response.on('error', (err) => reject(err))
-        })
-        request.on('error', (err) => reject(err))
-    })
-}
+const getRemoteData = (url) => new Promise((resolve, reject) => {
+    const client = url.startsWith('https') ? require('https') : require('http');
+    const request = client.get(url, (response) => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error(`Failed with status code: ${response.statusCode}`));
+      }
+      const body = [];
+      response.on('data', (chunk) => body.push(chunk));
+      response.on('end', () => resolve(body.join('')));
+    });
+    request.on('error', (err) => reject(err));
+  });
+
+  
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
