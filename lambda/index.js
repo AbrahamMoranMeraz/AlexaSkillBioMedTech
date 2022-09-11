b03600415ba4220e0caaee321f9f77ca838468d6
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-11 10:36:08 +0000

    updated lambda/index.js, lambda/package.json.

--- lambda/index.js
+++ lambda/index.js
@@ -4,19 +4,7 @@
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
-
-const getRemoteData = (url) => new Promise((resolve, reject) => {
-    const client = url.startsWith('https') ? require('https') : require('http');
-    const request = client.post(url, (response) => {
-      if (response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error(`Failed with status code: ${response.statusCode}`));
-      }
-      const body = [];
-      response.on('data', (chunk) => body.push(chunk));
-      response.on('end', () => resolve(body.join('')));
-    });
-    request.on('error', (err) => reject(err));
-  });
+const fetch = require('node-fetch');
 
 
 const LaunchRequestHandler = {
@@ -33,7 +21,6 @@ const LaunchRequestHandler = {
     }
 };
 
-const fetch = require('node-fetch');
 
 const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
